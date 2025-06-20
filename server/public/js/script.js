261cbe42264be51d174b6c414924e2d992e84e2b
sugammahara <sugammahara@gmail.com>
2025-06-20 23:23:46 +0545

    db

--- server/public/js/script.js
+++ server/public/js/script.js
@@ -1,65 +1,44 @@
 const socket = io();
 
-const map = L.map("map").setView([27.7172, 85.324], 16); // Default center
+if (navigator.geolocation){
+    navigator.geolocation.watchPosition(
+        (position)=>{
+        const { latitude, longitude } = position.coords;
+        socket.emit("send-location", { latitude, longitude } );
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "OpenStreetMap",
-}).addTo(map);
-
-const markers = {};
-let routingControl;
-const DESTINATION = [27.7172, 85.324]; // Kathmandu Durbar Square (example)
-
-if (navigator.geolocation) {
-  navigator.geolocation.watchPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-      socket.emit("send-location", { latitude, longitude });
     },
     (error) => {
-      console.error("Geolocation error:", error);
+        console.error(error);
     },
     {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximunAge: 0,
     }
-  );
-} else {
-  alert("Geolocation is not supported by your browser.");
+);
 }
+const map = L.map("map").setView([0, 0], 16);
 
-socket.on("receive-location", (data) => {
-  const { id, latitude, longitude } = data;
-  if (!latitude || !longitude) return;
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
+    attribution:"OpenStreetMap"
+}).addTo(map)
 
-  if (markers[id]) {
-    markers[id].setLatLng([latitude, longitude]);
-  } else {
-    markers[id] = L.marker([latitude, longitude]).addTo(map);
-  }
+const markers = {};
 
-  if (id === socket.id) {
+socket.on("receive-location", (data)=>{
+    const {id, latitude, longitude} = data;
     map.setView([latitude, longitude]);
+    if(markers[id]){
+        marker[id].setLatLng([latitude, longitude]);
 
-    if (routingControl) {
-      map.removeControl(routingControl);
+    }else{
+        markers[id]= L.marker([latitude, longitude ]).addTo(map);
     }
-
-    routingControl = L.Routing.control({
-      waypoints: [L.latLng(latitude, longitude), L.latLng(...DESTINATION)],
-      routeWhileDragging: false,
-      addWaypoints: false,
-      draggableWaypoints: false,
-      show: false,
-      createMarker: () => null,
-    }).addTo(map);
-  }
 });
 
-socket.on("user-disconnected", (id) => {
-  if (markers[id]) {
-    map.removeLayer(markers[id]);
-    delete markers[id];
-  }
+socket.on("user-disconnected", (id) =>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
 });
